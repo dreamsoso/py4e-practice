b316c2b80deafc209434014741d5f7cbd58f2b55
leon <leon8909@realtek.com>
2019-10-17 02:49:46 +0800

    Update dict_ex4.py

---
+++ dict/dict_ex4.py
@@ -0,0 +1,19 @@
+fname = input("Enter file name:")
+fhand = open(fname)
+d = dict()
+maxinum = None
+for line in fhand:
+    if line.startswith('From'):
+        words = line.split()
+        if len(words) > 1:
+            if words[1] in d:
+                d[words[1]]+=1
+            else:
+                d[words[1]]=1
+for key in d:
+    if maxinum is None or d[key] > maxinum:
+        maxinum = d[key]
+
+print(d)
+print(list(d.values()))
+print("max=%d" % maxinum)
