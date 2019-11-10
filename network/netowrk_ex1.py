a1022820561f2e3d660454fc2fbb9890434566da
leon <leon8909@realtek.com>
2019-11-11 07:11:03 +0800

    Upload netowrk_ex1

--- network/netowrk_ex1.py
+++ network/netowrk_ex1.py
@@ -2,12 +2,11 @@ import socket
 import re
 
 target_url = input("Enter URL:")
-HOST = re.findall('(^http[s+]://)',target_url)
-HOST = re.findall('(http[s]?://.*?)',target_url)
-print(HOST)
+#HOST = re.findall('(^http[s+]://)',target_url)
+HOST = re.findall("http[s]?://(.+)",target_url)
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    mysock.connect((HOST, 80))
+    mysock.connect((HOST[0], 80))
     mysock.close()
 except:
     print("socket connect fail!\n")
